9d365d74febef25f6eda503c80054950b051757d
markodimitrije <markodimitrije@gmail.com>
2018-08-30 18:04:32 +0200

    jos jedan initial commit nesto sam zabagovao sa git-om

--- QuizzWall/VCs/ViewController.swift
+++ QuizzWall/VCs/ViewController.swift
@@ -21,11 +21,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         loadQuestionsOnUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     private func loadQuestionsOnUI() {
 
